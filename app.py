1c6aae940590c20bc2e2fa0d5b658ab3399e4596
Oscar Jacobsen <67653883+oscarjacobsen@users.noreply.github.com>
2023-08-30 18:51:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World mafa!</p>"
+
+@app.route("/test")
+def test():
+    return "<p>Testing this amazing new route tjommi!</p>"
